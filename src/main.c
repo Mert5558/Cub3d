2ecc3701a071f72d9220ebdd09a403553e2b5541
Mert5558 <149884808+Mert5558@users.noreply.github.com>
2025-01-01 16:36:11 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -6,80 +6,17 @@
 /*   By: merdal <merdal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 11:46:26 by amecani           #+#    #+#             */
-/*   Updated: 2024/12/31 16:59:27 by merdal           ###   ########.fr       */
+/*   Updated: 2025/01/01 16:32:28 by merdal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "../includes/cub3d.h"
-
-
-
-int	get_rgba(int r, int g, int b, int a)
-{
-	int	rgba;
-
-	r = r << 24;
-	g = g << 16;
-	b = b << 8;
-	rgba = r | g | b | a;
-	return (rgba);
-}
-
-void	paint_floor_and_celing(mlx_t *window, int f_c, int c_c)
-{
-	uint32_t x;
-	uint32_t y;
-	mlx_image_t	*img;
-	int		half_heigh = HEIGHT / 2;
-
-	img = mlx_new_image(window, WIDTH, HEIGHT);
-	y = 0;
-	while ((int)y < half_heigh)
-	{
-		x = 0;
-		while (x < WIDTH)
-			mlx_put_pixel(img, x++, y, c_c);
-		y++;
-	}
-	while (y < HEIGHT)
-	{
-		x = 0;
-		while (x < WIDTH)
-			mlx_put_pixel(img, x++, y, f_c);
-		y++;
-	}
-	mlx_image_to_window(window, img, 0, 0);
-}
-
-void	putting_wasll_manuanylly(t_exec *exec_data)
-{
-	int i = 0;
-	if (			(exec_data->wall[0] = mlx_load_png("texutres/pentagon_wall.png")))
-		i++;
-	if (i == 1	&&	(exec_data->wall[1] = mlx_load_png("texutres/pentagon_wall.png")))
-		i++;
-	if (i == 2	&&	(exec_data->wall[2] = mlx_load_png("texutres/pentagon_wall.png")))
-		i++;
-	if (i == 3	&&	(exec_data->wall[3] = mlx_load_png("texutres/pentagon_wall.png")))
-		i++;
-
-	if (i++ != 4)
-	{
-		if (i == 1)
-			return;
-		while (--i != -1)
-			mlx_delete_texture(exec_data->wall[i]);
-	}
-}
 
 // int	leaks(void)
 // {
 // 	return (system("leaks cub3D"));
 // }
 // 	atexit((void *)leaks);
-
-#include <stdio.h> // For printf
 
 
 int main (int argc, char **argv)
@@ -92,11 +29,7 @@ int main (int argc, char **argv)
 		exit(1);
 	}
 	parser(&game, argv[1]);
-	// //Execution 
-	// excecution();
-	(void)argc;
-	(void)argv;
-	// Execution 
+
 	free_game(&game);
 	return (0);
 }
